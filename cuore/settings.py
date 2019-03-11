b38aa4295e7f95c6cbbfc202394dc12dc7d1d91d
Victor Perez <victor.rpt22@gmail.com>
2019-03-10 21:38:12 -0700

    Adding dashboard and patient modules

--- cuore/settings.py
+++ cuore/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cuore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cuore',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
